d117637ab3f799701cb0835297f73f4751222dc9
Abdul Majeed Kanoor <abdulkanoor@gmail.com>
2024-12-02 02:46:36 +0530

    initial commit

---
+++ department-service/src/main/java/com/dailycodebuffer/departmentservice/config/WebClientConfig.java
@@ -0,0 +1,39 @@
+package com.dailycodebuffer.departmentservice.config;
+
+import com.dailycodebuffer.departmentservice.client.EmployeeClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.reactive.LoadBalancedExchangeFilterFunction;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpExchangeAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class WebClientConfig {
+
+    @Autowired
+    private final LoadBalancedExchangeFilterFunction filterFunction;
+
+    public WebClientConfig(LoadBalancedExchangeFilterFunction filterFunction) {
+        this.filterFunction = filterFunction;
+    }
+
+    @Bean
+    public WebClient employeeWebClient() {
+        return WebClient.builder()
+                .baseUrl("http://employee-service")
+                .filter(filterFunction)
+                .build();
+    }
+
+    @Bean
+    public EmployeeClient employeeClient(WebClient webClient) {
+        HttpExchangeAdapter exchangeAdapter = WebClientAdapter.create(webClient);
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory
+                .builderFor(exchangeAdapter)
+                .build();
+        return factory.createClient(EmployeeClient.class);
+    }
+}
